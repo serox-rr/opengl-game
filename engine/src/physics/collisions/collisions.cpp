a9a8642a0f0979d57597f346d7f70d4fe104e10c
serox <serox@tutanota.com>
2024-05-11 13:03:56 +0200

    Added interpolation to collisions with the terrain

--- engine/src/physics/collisions/collisions.cpp
+++ engine/src/physics/collisions/collisions.cpp
@@ -7,20 +7,38 @@ module;
 module engine;
 
 namespace Engine::Physics {
-    std::optional<glm::vec3> checkCollision(const Entity *entity, const std::vector<std::reference_wrapper<const Renderable>> &renderables,
-                    const glm::vec3 &position) {
+    std::optional<glm::vec3> checkCollision(const Entity *entity,
+                                            const std::vector<std::reference_wrapper<const Renderable>> &renderables,
+                                            const glm::vec3 &position) {
         if (const auto terrain = dynamic_cast<const Terrain *>(&renderables[0].get())) {
             const auto &heightMap = terrain->getHeightMap();
             const auto width = terrain->getWidth();
             const auto height = terrain->getHeight();
-            const auto x = static_cast<int>(position.x) + width / 2;
-            const auto z = static_cast<int>(position.z) + height / 2;
-            if (x > width || z > height || x < 0 || z < 0)
+            float floorX1 = floor(position.x);
+            float floorZ1 = floor(position.z);
+            const int xHeightMap = floorX1 + width / 2;
+            const auto zHeightMap = floorZ1 + height / 2;
+            if (xHeightMap+1 > width || zHeightMap+1 > height || xHeightMap-1 < 0 || zHeightMap < 0)
                 return std::nullopt;
-
-            if (const double y = heightMap.at(z * width + x) * 64.0 + 1; y > position.y)
+            const auto y1 = heightMap.at(zHeightMap * width + xHeightMap);
+            const auto y2 = heightMap.at(zHeightMap * width + xHeightMap + 1);
+            const auto y3 = heightMap.at((zHeightMap + 1) * width + xHeightMap);
+            const auto y4 = heightMap.at((zHeightMap + 1) * width + xHeightMap + 1);
+            points->setVertices(
+                    {floorX1, static_cast<float>(heightMap.at(zHeightMap * width + xHeightMap) * 64.0 + 0.5),
+                     floorZ1, floor(position.x + 1),
+                     static_cast<float>(heightMap.at((zHeightMap + 1) * width + (xHeightMap + 1)) * 64.0 + 0.5),
+                     floor(position.z + 1), floor(position.x + 1),
+                     static_cast<float>(heightMap.at(zHeightMap * width + (xHeightMap + 1)) * 64.0 + 0.1),
+                     floorZ1, floorX1,
+                     static_cast<float>(heightMap.at((zHeightMap + 1) * width + xHeightMap) * 64.0 + 0.5),
+                     floor(position.z + 1)});
+            const auto y = (y1 + (y2 - y1) * (position.x - floorX1) +
+                           (y3 - y1) * (position.z - floorZ1) +
+                           (y1 + y4 - y2 - y3) * (position.x - floorX1) * (position.z - floorZ1))*64.0+1.0;
+            if (y > position.y)
                 return glm::vec3(position.x, y, position.z);
         }
         return std::nullopt;
     }
-} // namespace Engine::Collisions
+} // namespace Engine::Physics
